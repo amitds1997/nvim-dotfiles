ffd2e78c351edbdee04d1b6ed1114392a89296a7
Amit Singh <amitds1997@gmail.com>
2022-01-20 19:17:34 +0530

    Remove copilot

--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -1,8 +1,6 @@
 local editor = {}
 local conf = require('modules.editor.config')
 
-editor['github/copilot.vim'] = {}
-
 editor['nvim-telescope/telescope.nvim'] = {
 	cmd = 'Telescope',
 	config = conf.telescope,
